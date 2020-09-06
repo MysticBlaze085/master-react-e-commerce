52ca19d2e0451848702d915d8ce303e177ccfb18
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-09-06 14:33:57 -0500

    removed log

--- src/App.js
+++ src/App.js
@@ -25,15 +25,12 @@ class App extends React.Component {
                 const userRef = createUserProfileDocument(userAuth);
 
                 (await userRef).onSnapshot((snapShot) => {
-                    this.setState(
-                        {
-                            currentUser: {
-                                id: snapShot.id,
-                                ...snapShot.data(),
-                            },
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data(),
                         },
-                        () => console.log(this.state)
-                    );
+                    });
                 });
             }
             this.setState({ currentUser: userAuth });
