2c4f49b2ad1c8b8307abefa81158deefc0e5348a
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-09-06 23:39:25 -0500

    added checkout item features addItem and removeItem, and update redux actions and reducer methods

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,6 @@
 export const CartActionTypes = {
     TOGGLE_CART_HIDDEN: '[CART] TOGGLE_CART_HIDDEN',
     ADD_CART_ITEM: '[CART] Add Cart Item',
+    REMOVE_CART_ITEM: '[CART] Remove Cart Item',
+    CLEAR_ITEM_FROM_CART: '[CART] Remove Item from cart',
 };
